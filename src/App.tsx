baa0fbf784300ba9aa68876a3dfc5630a02772bb
ali-harrison <tewairama@proton.me>
2025-05-09 14:10:05 +1200

    css fixed , need to extend background

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,20 @@
-// App.tsx
 import React from 'react'
 import Home from './pages/Home'
 import './styles/App.css' // Ensure you have necessary styles
+import GrainBackground from './components/Grainbackground'
 
 function App() {
   return (
     <div className="outer-wrapper">
+      {/* Add the grain background */}
+      <GrainBackground
+        intensity={0.01}
+        blurAmount={0.5}
+        animate={true}
+        color1="#000000"
+        color2="#ffffff"
+      />
+
       <Home />
       <div className="corner-labels">© Crook</div>
     </div>
